46e8d80904d948ea078b0efe1e2e78dbc7405df9
Ciprian Mihaila <mcipri25@yahoo.com>
2014-05-17 19:22:30 +0300

    Tab Navigation MainActivity

--- client/Travel_Client/src/com/pdsd/project/main/MainActivity.java
+++ client/Travel_Client/src/com/pdsd/project/main/MainActivity.java
@@ -1,23 +1,45 @@
 package com.pdsd.project.main;
 
-import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private final static String MY_PLACES="My Places";
+	private final static String NEWS_FEED="News Feed";
+	ActionBar.Tab myPlacesTab,newsFeedTab;
+	MyPlacesFragment myPlacesFragment = new MyPlacesFragment();
+	NewsFeedFragment newsFeedFragment = new NewsFeedFragment();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ActionBar actionBar = getActionBar();
+		
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		myPlacesTab = actionBar.newTab();
+		newsFeedTab = actionBar.newTab();
+		myPlacesTab.setText(MY_PLACES);
+		newsFeedTab.setText(NEWS_FEED);
+		
+		myPlacesTab.setTabListener(new TabListener(myPlacesFragment));
+		newsFeedTab.setTabListener(new TabListener(newsFeedFragment));
+		
+		actionBar.addTab(myPlacesTab);
+		actionBar.addTab(newsFeedTab);
+		
+	}
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	
 }
