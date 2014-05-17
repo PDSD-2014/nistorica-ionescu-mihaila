82aad6d61d9570d691334087cf36e8a2751a2ec9
Ciprian Mihaila <mcipri25@yahoo.com>
2014-05-17 19:25:46 +0300

    My Places Tab Fragment

---
+++ client/Travel_Client/src/com/pdsd/project/main/MyPlacesFragment.java
@@ -0,0 +1,21 @@
+package com.pdsd.project.main;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class MyPlacesFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		LinearLayout listLayout = (LinearLayout) inflater.inflate(R.layout.my_places_list, container, false);
+		
+		return listLayout;
+	}
+	
+	
+}
