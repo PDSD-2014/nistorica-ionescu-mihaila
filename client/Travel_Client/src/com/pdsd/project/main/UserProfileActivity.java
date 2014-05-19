f05ab358df75275c7616bb6e7c21127ddfcf6407
Vlad Nistorica <vlad.nistorica@gmail.com>
2014-05-19 23:29:02 +0300

    Adaugare pagina informatii user

---
+++ client/Travel_Client/src/com/pdsd/project/main/UserProfileActivity.java
@@ -0,0 +1,137 @@
+package com.pdsd.project.main;
+
+
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import location.RatingListAdapter;
+import login.Session;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import comunication.ServerTask;
+import comunication.ServerTaskObject;
+
+public class UserProfileActivity extends Activity {
+	Spinner spinnerRating;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_user_profile);
+		Intent intent = getIntent();
+		final String user_id = intent.getStringExtra("username");
+		
+		spinnerRating = (Spinner) findViewById(R.id.userSpinner);
+		List<String> rates = new ArrayList<String>();
+		String[] ratesStr = {"1","2","3","4","5","6","7","8","9","10"};
+		rates = Arrays.asList(ratesStr);
+		ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,rates);
+		spinnerRating.setAdapter(spinnerAdapter);
+		
+		final Activity actv= this;
+		Button voteButton = (Button) findViewById(R.id.rateUser);
+		voteButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {				
+				String comentariu =((EditText) findViewById(R.id.addDescription)
+									).getText().toString();
+				String value=spinnerRating.getSelectedItem().toString() ;
+				
+				try {
+					comentariu=URLEncoder.encode(comentariu,"UTF-8");
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+				}
+				
+				Map<String,String> arg = new HashMap<String,String>();
+				arg.put("type","5");
+				arg.put("rated_entity_id",user_id);
+				arg.put("rating_type","0");
+				arg.put("value",value);
+				arg.put("body",comentariu);
+				arg.put("user_id",Session.getUserId(actv));
+				
+				ServerTaskObject serverTaskObj = new ServerTaskObject("post_new.php",arg,actv);
+				serverTaskObj.execute();
+			}
+		});
+		
+		Map<String,String> arguments = new HashMap<String,String>();
+		arguments.put("uid", user_id);
+		ServerTaskObject serverTask = new ServerTaskObject("get_user_info.php", arguments, this);
+		serverTask.execute();	
+		
+		Map<String,String> arg = new HashMap<String,String>();
+		arg.put("type","1");
+		arg.put("user_id",user_id);
+		
+		ServerTask serverTask2 = new ServerTask("get_rating.php",arg,this);
+		serverTask2.execute();
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.user_profile, menu);
+		return true;
+	}
+	
+	public static void onPostExecute(Activity act , JSONArray result){
+		ListView ratingList = (ListView) act.findViewById(R.id.userPostsList);
+		ratingList.setAdapter(new RatingListAdapter(act, result));
+	}
+	
+	public static void onPostExecute(Activity act, JSONObject result) {
+		TextView userField = (TextView) act.findViewById(R.id.user);
+		TextView ratingField = (TextView) act.findViewById(R.id.rating_value);
+		TextView numeField = (TextView) act.findViewById(R.id.nume);
+		TextView prenumeField = (TextView) act.findViewById(R.id.prenume);
+		TextView mailField = (TextView) act.findViewById(R.id.mail);
+		
+		try {
+			if (!result.getString("status").equals("error")) {
+				JSONObject message = result.getJSONObject("message");
+
+				String username = message.getString("username");
+				String nume = message.getString("nume");
+				String prenume = message.getString("prenume");
+				String mail = message.getString("mail");
+				String rating = message.getString("rating");
+
+				userField.setText(username);
+				ratingField.setText(rating);
+				numeField.setText(nume);
+				prenumeField.setText(prenume);
+				mailField.setText(mail);
+			}
+			else {
+				Common.printError(act, "Could not retrive user information");
+			}
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
