f455a10ae044934c6dca5369d5bee6862590bc90
Vlad Nistorica <vlad.nistorica@gmail.com>
2014-05-21 15:33:24 +0300

    Afisare camp url in orice mesaj

--- server/get_update.php
+++ server/get_update.php
@@ -5,19 +5,15 @@
 	if($db->connect_error){
 		die("CONECTION ERROR (".$db->connect_errno.")".$db->connect_error);
 	}
-	
-	
 	if(!isset($_GET['type'])){
 		die("Incorrect message");
 	}
  	$message = $_GET;
 	$type = $message['type'];
 	switch($type){
-		
 		case '1' : {
 			$user_id = $message['user_id'];
-			
-			$query = "SELECT l.location_name,l.location_id , up.* , users.username FROM updates up 
+			$query = "SELECT l.location_name,l.location_id , up.* , users.username FROM updates up
 																		INNER JOIN locations l
 											   							INNER JOIN user_places us
 																		INNER JOIN users
@@ -26,12 +22,10 @@
 																		l.location_id = us.location_id AND
 																		users.uid = up.user_id
 																		ORDER BY up.date DESC";
-			
-			
 			$result = $db->query($query);
 			if(!$result) {$response=null; break;}
 			$response=array();
-			$count=0;		
+			$count=0;
 			while($x=mysqli_fetch_array($result)){
 				$line = array();
 				$line['location_id'] = $x['location_id'];
@@ -46,29 +40,26 @@
 				if($x['type']!=1){
 					$line['url'] = $x['url'];
 				}
-				
+        else {
+          $line['url'] = "";
+        }
 				$response[$count] = $line;
 				$count+=1;
 			}
-			
 		}break;
-			
 		case '2' : {
 			$user_id = $message['user_id'];
-			
-			$query = "SELECT l.location_name,l.location_id , up.* , users.username FROM updates up 
+			$query = "SELECT l.location_name,l.location_id , up.* , users.username FROM updates up
 																		INNER JOIN locations l
 																		INNER JOIN users
 																		WHERE up.location_id = l.location_id AND
 																		users.uid = up.user_id
 																		ORDER BY up.date DESC";
-			
-			
 
 			$result = $db->query($query);
 			if(!$result) {$response=null; break;}
 			$response=array();
-			$count=0;		
+			$count=0;
 			while($x=mysqli_fetch_array($result)){
 				$line = array();
 				$line['location_id'] = $x['location_id'];
@@ -83,16 +74,16 @@
 				if($x['type']!=1){
 					$line['url'] = $x['url'];
 				}
-				
+        else {
+          $line['url'] = "";
+        }
 				$response[$count] = $line;
 				$count+=1;
-			}			
+			}
 		}break;
-	
 		case '3' : {
 			$location_id = $message['location_id'];
-			
-			$query = "SELECT  up.* , users.username FROM updates up 
+			$query = "SELECT  up.* , users.username FROM updates up
 													INNER JOIN users
 															WHERE up.location_id = '".$location_id."' AND
 															users.uid = up.user_id
@@ -100,7 +91,7 @@
 			$result = $db->query($query);
 			if(!$result) {$response=null; break;}
 			$response=array();
-			$count=0;		
+			$count=0;
 			while($x=mysqli_fetch_array($result)){
 				$line = array();
 				$line['update_id'] = $x['update_id'];
@@ -113,17 +104,17 @@
 				if($x['type']!=1){
 					$line['url'] = $x['url'];
 				}
-				
+        else {
+          $line['url'] = "";
+        }
 				$response[$count] = $line;
 				$count+=1;
-			}																
-			
+			}
 		}break;
 	/*
 		case '4' : {}break;
 	*/
 	}
-	
 	$reply = json_encode($response);
 	echo $reply;
 ?>
