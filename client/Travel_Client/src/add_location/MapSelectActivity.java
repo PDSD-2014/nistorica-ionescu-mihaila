db8c1482fc82ffe78245831d0abdeef4f3282870
Vlad Nistorica <vlad.nistorica@gmail.com>
2014-05-21 14:18:47 +0300

    Solved conflicts

--- client/Travel_Client/src/add_location/MapSelectActivity.java
+++ client/Travel_Client/src/add_location/MapSelectActivity.java
@@ -1,17 +1,16 @@
 package add_location;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.pdsd.project.main.R;
 
 public class MapSelectActivity extends FragmentActivity {
